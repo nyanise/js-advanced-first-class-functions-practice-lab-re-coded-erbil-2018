ced80edf7d20c2f426b4bb560eb755ce62468119
Neyan najat <nyannajat8@gmail.com>
2018-07-26 08:13:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
-const logDriversByHometown= function(drivers,homeTown)
-{
-   drivers.forEach(function(driver){
-     if(driver.hometown===homeTown)
-     console.log(driver.name)
-     
-   })
-}
-const driversByRevenue = function(drivers)
-{
-  return drivers.slice().sort(function(driver1,driver2){
-    return driver1.revenue - driver2.revenue;
-   
+// Code your solution in this file!
+
+const logDriverNames=function(drivers){
+  drivers.forEach(function(driver){
+    console.log(driver.name)
   })
-}
-const driversByName = function(drivers)
-{
-  return driver.slice().sort(function(driver1,driver2)
-  {
-    return driver1.name.localeCompare(driver2.name);
-  })
-}
-const totallRevenue = function(drivers)
-{
-  let totall=0;
-  driver.forEach(function(driver){totall+=driver.revenue})
 }
 
+const logDriversByHometown=function(drivers,hometown){
+  console.log(driver.name)
+  drivers.forEach(function (driver){
+    if(driver.hometown===hometown)
+      console.log(driver.name)
+    })
+
+}
+const driversByRevenue=function(drivers){
+  return driver.sort()
+}
