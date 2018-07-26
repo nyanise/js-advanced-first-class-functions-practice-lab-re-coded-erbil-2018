cc0800b095a88d3fd108d7809652fcde30b1447c
Neyan najat <nyannajat8@gmail.com>
2018-07-26 08:15:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,11 +6,31 @@ const logDriverNames=function(drivers){
   })
 }
 
-const logDriversByHometown=function(drivers,hometown){
-  console.log(driver.name)
-  drivers.forEach(function (driver){
-    if(driver.hometown===hometown)
-      console.log(driver.name)
-    })
 
 }
+const logDriversByHometown= function(drivers,homeTown)
+{
+   drivers.forEach(function(driver){
+     if(driver.hometown===homeTown)
+     console.log(driver.name)
+     
+   })
+}
+const driversByRevenue = function(drivers)
+{
+  return drivers.slice().sort(function(driver1,driver2){
+    return driver1.revenue - driver2.revenue;
+   
+  })
+}
+const driversByName = function(drivers)
+{
+  return driver.slice().sort(function(driver1,driver2)
+  {
+    return driver1.name.localeCompare(driver2.name);
+  })
+}
+const totallRevenue = function(drivers)
+{
+  let totall=0;
+  driver.forEach(function(driver){totall+=driver.revenue})
