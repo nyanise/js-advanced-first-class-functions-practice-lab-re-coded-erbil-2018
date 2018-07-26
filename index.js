c69beb72ae306cf1dacf438ccd6961c3d8789a92
Neyan najat <nyannajat8@gmail.com>
2018-07-26 09:37:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,93 +1,35 @@
-const expect = chai.expect;
-
-describe('index.js', function () {
-  const drivers = [
-    { name: 'Bobby',   hometown: 'Pittsburgh',  revenue: 3000 },
-    { name: 'Sally',   hometown: 'New York',    revenue: 2000 },
-    { name: 'Sammy',   hometown: 'Pittsburgh',  revenue: 2500 },
-    { name: 'Annette', hometown: 'Los Angeles', revenue: 6000 },
-    { name: 'Bobby',   hometown: 'Tampa Bay',   revenue: 5000 }
-  ];
-
-  const driversCopy = [...drivers];
-
-  afterEach(function () {
-    expect(drivers, 'MAKE SURE YOUR ARRAY MANIPULATIONS ARE NON-DESTRUCTIVE').to.eql(driversCopy);
+const logDriverNames = function (drivers) {
+  drivers.forEach(function (driver) {
+    console.log(driver.name);
   });
+};
 
-  describe('logDriverNames()', function () {
-    let spy;
-
-    beforeEach(function () {
-      spy = sinon.spy(console, 'log');
-    });
-
-    afterEach(function () {
-      spy.restore();
-    });
-
-    it('logs the name of each driver', function () {
-      logDriverNames(drivers);
-
-      expect(spy.calledWithExactly('Bobby')).to.be.true;
-
-      expect(spy.calledWithExactly('Sally')).to.be.true;
-
-      expect(spy.calledWithExactly('Sammy')).to.be.true;
-
-      expect(spy.calledWithExactly('Annette')).to.be.true;
-
-      expect(spy.calledWithExactly('Bobby')).to.be.true;
-    });
+const logDriversByHometown = function (drivers, hometown) {
+  drivers.forEach(function (driver) {
+    if (driver.hometown === hometown) {
+      console.log(driver.name);
+    }
   });
+};
 
-  describe('logDriversByHometown()', function () {
-    let spy;
-
-    beforeEach(function () {
-      spy = sinon.spy(console, 'log');
-    });
-
-    afterEach(function () {
-      spy.restore();
-    });
-
-    it('logs the name of each driver whose hometown matches the passed-in argument', function () {
-      logDriversByHometown(drivers, 'Pittsburgh');
-
-      expect(spy.calledWithExactly('Bobby')).to.be.true;
-
-      expect(spy.calledWithExactly('Sammy')).to.be.true;
-
-      expect(spy.calledWithExactly('Annette')).to.not.be.true;
-    });
+const driversByRevenue = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.revenue - driverTwo.revenue;
   });
-  
-  describe('driversByRevenue()', function () {
-    it('uses the sort() method to return a new array of drivers ordered by revenue (lowest to highest)', function () {
-      expect(driversByRevenue(drivers)[0].name).to.eql('Sally');
+};
 
-      expect(driversByRevenue(drivers)[drivers.length - 1].name).to.eql('Annette');
-    });
+const driversByName = function (drivers) {
+  return drivers.slice().sort(function (driverOne, driverTwo) {
+    return driverOne.name.localeCompare(driverTwo.name);
   });
+};
 
-  describe('driversByName()', function () {
-    it('uses the sort() method to return a new array of drivers ordered alphabetically by name (A to Z)', function () {
-      expect(driversByName(drivers)[0].name).to.eql('Annette');
+const totalRevenue = function (drivers) {
+  return drivers.reduce(function (total, currentDriver) {
+    return currentDriver.revenue + total;
+  }, 0);
+};
 
-      expect(driversByName(drivers)[drivers.length - 1].name).to.eql('Sammy');
-    });
-  });
-
-  describe('totalRevenue()', function () {
-    it('uses the reduce() method to sum the revenue of every driver and return the total', function () {
-      expect(totalRevenue(drivers)).to.equal(18500);
-    });
-  });
-
-  describe('averageRevenue()', function () {
-    it('calculates the average revenue across all drivers', function () {
-      expect(averageRevenue(drivers)).to.equal(3700);
-    });
-  });
-});
+const averageRevenue = function (drivers) {
+  return totalRevenue(drivers) / drivers.length;
+};
