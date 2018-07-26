0d9799f6f8a91ee4c6c3d81d79f25f338597da07
Neyan najat <nyannajat8@gmail.com>
2018-07-26 08:12:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+
+const logDriverNames=function(drivers){
+  drivers.forEach(function(driver){
+    console.log(driver.name)
+  })
+}
+
+const logDriversByHometown=function(drivers,hometown){
+  console.log(driver.name)
+  drivers.forEach(function (driver){
+    if(driver.hometown===hometown)
+      console.log(driver.name)
+    })
+
+}
+const driversByRevenue=function(drivers){
+  return driver.sort()
+}
